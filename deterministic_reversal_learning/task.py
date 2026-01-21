72984ed0fd30351ca6b08a002eaf5364ccd5c81d
Florian Rau <bimac@users.noreply.github.com>
2026-01-21 13:23:37 +0000

    Revert "inherit from `TrainingChoiceWorldSession` instead of `ActiveChoiceWorldSession`"
    This reverts commit d51cf3f4ed01de5ecfdbcece6da021438c53fc76.

--- deterministic_reversal_learning/task.py
+++ deterministic_reversal_learning/task.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import yaml
 
 from iblrig.base_choice_world import (
-    TrainingChoiceWorldSession,
-    TrainingChoiceWorldTrialData,
+    ActiveChoiceWorldSession,
+    ActiveChoiceWorldTrialData,
 )
 from iblrig.misc import get_task_arguments
 
@@ -16,11 +16,11 @@ with open(Path(__file__).parent.joinpath("task_parameters.yaml")) as f:
     DEFAULTS = yaml.safe_load(f)
 
 
-class DeterministicReversalLearningTrialData(TrainingChoiceWorldTrialData):
+class DeterministicReversalLearningTrialData(ActiveChoiceWorldTrialData):
     pass
 
 
-class Session(TrainingChoiceWorldSession):
+class Session(ActiveChoiceWorldSession):
     protocol_name = "DeterministicReversalLearning"
     TrialDataModel = DeterministicReversalLearningTrialData
     # TODO: Implementation
