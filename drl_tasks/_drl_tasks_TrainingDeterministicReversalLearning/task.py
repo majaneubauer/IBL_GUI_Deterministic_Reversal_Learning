f36dfa8696898b849fd2282ffe639d72ffbbe36c
majaneubauer <neubauer.maja@outlook.de>
2026-04-28 14:56:48 +0200

    attempt at training DRL

---
+++ drl_tasks/_drl_tasks_TrainingDeterministicReversalLearning/task.py
@@ -0,0 +1,69 @@
+import logging
+from pathlib import Path
+import yaml
+from iblrig.misc import get_task_arguments
+
+from drl_tasks.deterministic_reversal_learning import (
+    DeterministicReversalLearningSession,
+)
+
+log = logging.getLogger(__name__)
+
+# read defaults from task_parameters.yaml
+with open(Path(__file__).parent.joinpath("task_parameters.yaml")) as f:
+    DEFAULTS = yaml.safe_load(f)
+
+
+class Session(DeterministicReversalLearningSession):
+    protocol_name = (
+        "DeterministicReversalLearning"  # here defined how it shows up in GUI
+    )
+
+    def __init__(
+        self,
+        *args,
+        reward_amount_ul: float = DEFAULTS["REWARD_AMOUNT_UL"],
+        stim_gain: float = DEFAULTS["STIM_GAIN"],
+        stop_miniscope_secs: int = DEFAULTS["STOP_MINISCOPE_SECS"],
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.task_params["REWARD_AMOUNT_UL"] = reward_amount_ul
+        self.task_params["STIM_GAIN"] = stim_gain
+        self.task_params["STOP_MINISCOPE_SECS"] = stop_miniscope_secs
+
+    @staticmethod
+    def extra_parser():
+        """:return: argparse.parser()"""
+        parser = super(Session, Session).extra_parser()
+        parser.add_argument(
+            "--reward_amount_ul",
+            option_strings=["--reward_amount_ul"],
+            dest="reward_amount_ul",
+            default=DEFAULTS["REWARD_AMOUNT_UL"],
+            type=float,
+            help="Reward amount (µl) within one session",
+        )
+        parser.add_argument(
+            "--stim_gain",
+            option_strings=["--stim_gain"],
+            dest="stim_gain",
+            default=DEFAULTS["STIM_GAIN"],
+            type=float,
+            help=f'Visual angle/wheel displacement (deg/mm, default: {DEFAULTS["STIM_GAIN"]})',
+        )
+        parser.add_argument(
+            "--stop_miniscope_secs",
+            option_strings=["--stop_miniscope_secs"],
+            dest="stop_miniscope_secs",
+            default=DEFAULTS["STOP_MINISCOPE_SECS"],
+            type=int,
+            help="Length of HIGH signal to stop miniscope at the end of one session",
+        )
+        return parser
+
+
+if __name__ == "__main__":  # pragma: no cover
+    kwargs = get_task_arguments(parents=[Session.extra_parser()])
+    sess = Session(**kwargs)
+    sess.run()
--- drl_tasks/deterministic_reversal_learning.py
+++ drl_tasks/deterministic_reversal_learning.py
@@ -810,6 +810,301 @@ class DeterministicReversalLearningSession(
         self.map_data.append(map_probability)  # add latest MAP
 
 
+class TrainingDeterministicReversalLearningTrialData(ActiveChoiceWorldTrialData):
+    """Pydantic Model for Trial Data, extended from :class:`~.iblrig.base_choice_world.ActiveChoiceWorldTrialData`."""
+
+    block_side: int  # -1 for left or +1 for right
+    stim_end_position: int
+
+
+class TrainingDeterministicReversalLearningSession(
+    DeterministicReversalLearningBaseSession, ActiveChoiceWorldSession
+):
+    protocol_name = (
+        "TrainingDeterministicReversalLearning"  # here defined how it shows up in GUI
+    )
+    TrialDataModel = TrainingDeterministicReversalLearningTrialData
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # to help bonsai find Gabor2D_MN.bonsai file
+        self.paths["VISUAL_STIM_FOLDER"] = self.get_task_directory().parent.parent
+        self.block_side = int(
+            np.random.choice(
+                self.task_params.BLOCK_SIDES,
+                p=[
+                    self.task_params.PROBABILITY_LEFT,
+                    1 - self.task_params.PROBABILITY_LEFT,
+                ],
+            )
+        )  # -1 = left, +1 = right
+
+    @property
+    def correct_end_position(self):
+        return int(
+            self.block_side * abs(self.task_params.STIM_END_POSITIONS[0])
+        )  # left block: -1 * |-35| = -35; right block: 1 * |-35| = 35
+
+    @property
+    def event_error(self):
+        return self.device_rotary_encoder.THRESHOLD_EVENTS[
+            (1 if self.task_params.STIM_REVERSE else -1) * self.correct_end_position
+        ]
+
+    @property
+    def event_reward(self):
+        return self.device_rotary_encoder.THRESHOLD_EVENTS[
+            (-1 if self.task_params.STIM_REVERSE else 1) * self.correct_end_position
+        ]
+
+    def draw_next_trial_info(self, *args, **kwargs):
+        # log block side
+        self.trials_table.at[self.trial_num, "block_side"] = self.block_side
+        # add stim end position to trials table
+        self.trials_table.at[self.trial_num, "stim_end_position"] = (
+            self.correct_end_position
+        )
+        super().draw_next_trial_info()
+
+    def get_state_machine_trial(self, i):
+        # we define the trial number here for subclasses that may need it
+        sma = self._instantiate_state_machine(trial_number=i)
+
+        # Signal trial start and stop all sounds
+        sma.add_state(
+            state_name="trial_start",
+            state_timer=0,  # ~100µs hardware irreducible delay
+            state_change_conditions={"Tup": "reset_rotary_encoder"},
+            output_actions=[self.bpod.actions.stop_sound, ("BNC1", 255)],
+        )
+
+        # Reset the rotary encoder by sending the following opcodes via the modules serial interface
+        # - 'Z' (ASCII 90): Set current rotary encoder position to zero
+        # - 'E' (ASCII 69): Enable all position thresholds (that may have been disabled by a threshold-crossing)
+        # cf. https://sanworks.github.io/Bpod_Wiki/serial-interfaces/rotary-encoder-module-serial-interface/
+        sma.add_state(
+            state_name="reset_rotary_encoder",
+            state_timer=0,
+            output_actions=[self.bpod.actions.rotary_encoder_reset],
+            state_change_conditions={"Tup": "quiescent_period"},
+        )
+
+        # Quiescent Period. If the wheel is moved past one of the thresholds: Reset the rotary encoder and start over.
+        # Continue with the stimulation once the quiescent period has passed without triggering movement thresholds.
+        sma.add_state(
+            state_name="quiescent_period",
+            state_timer=self.quiescent_period,
+            output_actions=[],
+            state_change_conditions={
+                "Tup": "stim_on",
+                self.movement_left: "reset_rotary_encoder",
+                self.movement_right: "reset_rotary_encoder",
+            },
+        )
+
+        # Show the visual stimulus. This is achieved by sending a time-stamped byte-message to Bonsai via the Rotary
+        # Encoder Module's ongoing USB-stream. Move to the next state once the Frame2TTL has been triggered, i.e.,
+        # when the stimulus has been rendered on screen. Use the state-timer as a backup to prevent a stall.
+        sma.add_state(
+            state_name="stim_on",
+            state_timer=0.1,
+            output_actions=[self.bpod.actions.bonsai_show_stim],
+            state_change_conditions={
+                "BNC1High": "interactive_delay",
+                "BNC1Low": "interactive_delay",
+                "Tup": "interactive_delay",
+            },
+        )
+
+        # Defined delay between visual and auditory cue
+        sma.add_state(
+            state_name="interactive_delay",
+            state_timer=self.task_params.INTERACTIVE_DELAY,
+            output_actions=[],
+            state_change_conditions={"Tup": "play_instructive_tone"},
+        )
+
+        # Play instrucive tone. Move to next state if sound is detected. Use the state-timer as a backup to prevent a stall.
+        sma.add_state(
+            state_name="play_instructive_tone",
+            state_timer=0.1,
+            output_actions=[self.bpod.actions.play_instructive_tone],
+            state_change_conditions={
+                "Tup": "open_loop",
+                "BNC2High": "open_loop",
+            },
+        )
+
+        # Start the open loop state in which the animal can move the wheel but wheel movement is not coupled to the stimulus position.
+        sma.add_state(
+            state_name="open_loop",
+            state_timer=self.task_params.DECISION_PERIOD_SECS,
+            output_actions=[],
+            state_change_conditions={"Tup": "play_go_tone"},
+        )
+
+        # Play go tone. Move to next state if sound is detected. Use the state-timer as a backup to prevent a stall.
+        sma.add_state(
+            state_name="play_go_tone",
+            state_timer=0.1,
+            output_actions=[self.bpod.actions.play_tone],
+            state_change_conditions={
+                "Tup": "reset2_rotary_encoder",
+                "BNC2High": "reset2_rotary_encoder",
+            },
+        )
+
+        # Reset rotary encoder (see above). Move on after brief delay (to avoid a race conditions in the bonsai flow).
+        sma.add_state(
+            state_name="reset2_rotary_encoder",
+            state_timer=0.05,
+            output_actions=[self.bpod.actions.rotary_encoder_reset],
+            state_change_conditions={"Tup": "closed_loop"},
+        )
+
+        # Start the closed loop state in which the animal controls the position of the visual stimulus by means of the
+        # rotary encoder. The three possible outcomes are:
+        # 1) wheel has NOT been moved past a threshold: continue with no-go condition
+        # 2) wheel has been moved in WRONG direction: continue with error condition
+        # 3) wheel has been moved in CORRECT direction: continue with reward condition
+
+        sma.add_state(
+            state_name="closed_loop",
+            state_timer=self.task_params.RESPONSE_WINDOW,
+            output_actions=[self.bpod.actions.bonsai_closed_loop],
+            state_change_conditions={
+                "Tup": "no_go",
+                self.event_error: "freeze_error",
+                self.event_reward: "freeze_reward",
+            },
+        )
+        # Error: Freeze the stimulus
+        sma.add_state(
+            state_name="freeze_error",
+            state_timer=0,
+            output_actions=[self.bpod.actions.bonsai_freeze_stim],
+            state_change_conditions={"Tup": "reward"},
+        )
+
+        # Reward: open the valve for a defined duration (and set BNC1 to high), freeze stimulus (freeze_reward same as
+        # freeze_error but how to differentiate which state to go to when Tup?).
+        # Continue to hide_stim/exit_state once FEEDBACK_CORRECT_DELAY_SECS have passed.
+        sma.add_state(
+            state_name="freeze_reward",
+            state_timer=0,
+            output_actions=[self.bpod.actions.bonsai_freeze_stim],
+            state_change_conditions={"Tup": "reward"},
+        )
+        sma.add_state(
+            state_name="reward",
+            state_timer=self.reward_time,
+            output_actions=[("Valve1", 255), ("BNC1", 255)],
+            state_change_conditions={"Tup": "correct"},
+        )
+        sma.add_state(
+            state_name="correct",
+            state_timer=self.feedback_correct_delay - self.reward_time,
+            output_actions=[],
+            state_change_conditions={"Tup": "hide_stim"},
+        )
+
+        # Hide the visual stimulus. This is achieved by sending a time-stamped byte-message to Bonsai via the Rotary
+        # Encoder Module's ongoing USB-stream. Move to the next state once the Frame2TTL has been triggered, i.e.,
+        # when the stimulus has been rendered on screen. Use the state-timer as a backup to prevent a stall.
+        sma.add_state(
+            state_name="hide_stim",
+            state_timer=0.1,
+            output_actions=[self.bpod.actions.bonsai_hide_stim],
+            state_change_conditions={
+                "Tup": "exit_state",
+                "BNC1High": "exit_state",
+                "BNC1Low": "exit_state",
+            },
+        )
+
+        # Wait for ITI_DELAY_SECS before ending the trial. Raise BNC1 to mark this event.
+        sma.add_state(
+            state_name="exit_state",
+            state_timer=self.task_params.ITI_DELAY_SECS,
+            output_actions=[("BNC1", 255)],
+            state_change_conditions={"Tup": "exit"},
+        )
+
+        return sma
+
+    def trial_completed(self, bpod_data: dict) -> None:
+        # removed assertion error for position = 0 cause that is what we want
+        # Get the response time from the behaviour data.
+        # It is defined as the time passing between the end of `open_loop` and the end of `closed_loop`.
+        state_times = bpod_data["States timestamps"]
+        response_time = (
+            state_times["closed_loop"][0][1] - state_times["open_loop"][0][1]
+        )
+        self.trials_table.at[self.trial_num, "response_time"] = response_time
+
+        try:
+            # Get the trial's outcome, i.e., the states that have a matching name and a valid time-stamp
+            # Assert that we have exactly one outcome
+            outcome_names = [
+                "correct",
+                "omit_correct",
+            ]
+            outcomes = [
+                name
+                for name, times in state_times.items()
+                if name in outcome_names and ~np.isnan(times[0][0])
+            ]
+            if (n_outcomes := len(outcomes)) != 1:
+                trial_states = "Trial states: " + ", ".join(
+                    k for k, v in state_times.items() if ~np.isnan(v[0][0])
+                )
+                assert (
+                    n_outcomes != 0
+                ), f"No outcome detected for trial {self.trial_num}.\n{trial_states}"
+                assert (
+                    n_outcomes == 1
+                ), f"{n_outcomes} outcomes detected for trial {self.trial_num}.\n{trial_states}"
+            outcome = outcomes[0]
+
+        except AssertionError as e:
+            # write bpod_data to disk, log exception then raise
+            self.save_trial_data_to_json(bpod_data, validate=False)
+            for line in re_split(r"\n", e.args[0]):
+                log.error(line)
+            raise e
+
+        # record the trial's outcome in the trials_table
+        self.trials_table.at[self.trial_num, "trial_correct"] = "correct" in outcome
+        if "correct" in outcome:
+            self.session_info.NTRIALS_CORRECT += 1
+            self.trials_table.at[self.trial_num, "response_side"] = np.sign(
+                self.correct_end_position
+            )  # np.sign returns 1 for positive and -1 for negative numbers
+
+        # Only run cleanup if last trial or user pressed stop
+        if self.stopped or self.trial_num >= (self.task_params.NTRIALS - 1):
+            self.stop_miniscope()
+
+        super(ActiveChoiceWorldSession, self).trial_completed(bpod_data)
+
+    def show_trial_log(
+        self, extra_info: dict[str, Any] | None = None, log_level: int = logging.INFO
+    ):
+        # construct info dict
+        trial_info = self.trials_table.iloc[self.trial_num]
+        info_dict = {
+            "Block Side": f"{trial_info.block_side}",
+            "Correct End Position": self.correct_end_position,
+        }
+
+        # update info dict with extra_info dict
+        if isinstance(extra_info, dict):
+            info_dict.update(extra_info)
+
+        # call parent method
+        super().show_trial_log(extra_info=info_dict, log_level=log_level)
+
+
 class HabituationDeterministicReversalLearningTrialData(ChoiceWorldTrialData):
     """Pydantic Model for Trial Data, extended from :class:`~.iblrig.base_choice_world.ChoiceWorldTrialData`."""
 
